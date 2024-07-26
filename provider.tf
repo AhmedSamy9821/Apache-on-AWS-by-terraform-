817e03c3ee364efc04ccd542dfda995cb9143bbc
AhmedSamy9821 <134964599+AhmedSamy9821@users.noreply.github.com>
2024-07-26 15:51:38 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAY2GJMVWMEV6A3N62"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
